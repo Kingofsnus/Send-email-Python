354737ba8b353289d99285da81980538018bcfc5
Kingofsnus <bog2007dan@icloud.com>
2021-05-23 21:10:51 +0300

    Create senderV1.py

---
+++ senderV1.py
@@ -0,0 +1,33 @@
+import smtplib
+
+#Who take u mail
+S_reciver = ''
+#You gmail data
+R_sender = ''
+password = ''
+#Title
+subject = ''
+#Text
+body = ''
+
+#Text 
+message = f'''From: {R_sender}
+To: {S_reciver}
+Subject: {subject}\n
+{body}
+'''
+
+#send
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    #Login
+    server.login(R_sender,password)
+    print('loged in ')
+    server.sendmail(R_sender,S_reciver,message)
+    print('U send message')
+
+
+except smtplib.SMTPNotSupportedError:
+    print('Support error')
